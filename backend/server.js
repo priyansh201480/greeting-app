c1fc56772b6324b4cda15f1c8ba7f567693acd90
Priyansh Agarwal <140069604+priyansh201480@users.noreply.github.com>
2025-03-07 20:49:43 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const express = require("express")
 const app = express();
 const cors = require('cors')
 app.use(cors());
+app.use(express.json());  
 
 app.get('/api/greet', (req, res) => {
   const name = req.query.name;
